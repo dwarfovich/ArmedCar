d569461366f463229f58d153ba8935b79f219af6
Tim Gilevich <ouncetim@gmail.com>
2025-01-05 15:08:54 +0300

    WIP

--- src/main.cpp
+++ src/main.cpp
@@ -11,24 +11,24 @@ constexpr unsigned long baudRate                = 57600;
 constexpr unsigned long serialMonitorStartDelay = 300;
 constexpr unsigned long readControllerDataDelay = 50;
 
-constexpr int pliersPin = 9;
-constexpr int handRotationPin = 14;
-constexpr int handVerticalPin1 = 15;
-constexpr int handVericalPin2 = 8;
-constexpr int leftWheelMotorPin = 3;
+constexpr int pliersPin          = 9;
+constexpr int handRotationPin    = 14;
+constexpr int handVerticalPin1   = 15;
+constexpr int handVerticalPin2    = 8;
+constexpr int leftWheelMotorPin  = 3;
 constexpr int rightWheelMotorPin = 5;
 
-ps2::Controller ps2;
-int           error          = 0;
-byte          controllerType = 0;
-byte          vibrate        = 0;
+ps2::Controller     controller;
+ps2::ErrorCode      error;
+ps2::ControllerType controllerType;
+byte                vibrate = 0;
 
 void setup()
 {
     Serial.begin(baudRate);
     delay(serialMonitorStartDelay);
-    error = ps2.configure(clockPin, commandPin, selectPin, dataPin, pressureMode, enableRumble);
-    if (error == 0) {
+    error = controller.configure(clockPin, commandPin, selectPin, dataPin, pressureMode, enableRumble);
+    if (error == ps2::ErrorCode::Success) {
         Serial.println("Found Controller, configured successful ");
         Serial.println("pressures = ");
         if (pressureMode)
@@ -44,113 +44,118 @@ void setup()
         Serial.println("Try out all the buttons, X will vibrate the controller, faster as you press harder;");
         Serial.println("holding L1 or R1 will print out the analog stick values.");
         Serial.println("Note: Go to www.billporter.info for updates and to report bugs.");
-    } else if (error == 1) {
+    } else if (error == ps2::ErrorCode::WrongControllerMode) {
         Serial.println("No controller found, check wiring, see readme.txt to enable debug. visit www.billporter.info "
                        "for troubleshooting tips");
-    } else if (error == 2) {
+    } else if (error == ps2::ErrorCode::ControllerNotAcceptingCommands) {
         Serial.println("Controller found but not accepting commands. see readme.txt to enable debug. Visit "
                        "www.billporter.info for troubleshooting tips");
-    } else if (error == 3) {
+    } else if (error == ps2::ErrorCode::PressureModeError) {
         Serial.println("Controller refusing to enter Pressures mode, may not support it. ");
     }
 
-    controllerType = ps2.type();
-    switch (controllerType) {
-        case 0: Serial.print("Unknown Controller type found "); break;
-        case 1: Serial.print("DualShock Controller found "); break;
-        case 2: Serial.print("GuitarHero Controller found "); break;
-        case 3: Serial.print("Wireless Sony DualShock Controller found "); break;
+    controllerType = controller.type();
+    if (controllerType == ps2::ControllerType::WirelessDualShock) {
+        Serial.print("Wireless Sony DualShock Controller found.");
+    } else {
+        Serial.print("Wrong controller type.");
     }
+    // switch (controllerType) {
+    //     case 0: Serial.print("Unknown Controller type found "); break;
+    //     case 1: Serial.print("DualShock Controller found "); break;
+    //     case 2: Serial.print("GuitarHero Controller found "); break;
+    //     case 3: Serial.print("Wireless Sony DualShock Controller found "); break;
+    // }
 }
 
 void loop()
 {
-    if (error == 1)
+    if (error == ps2::ErrorCode::WrongControllerMode)
         return;
-    if (error == 2) {
-        ps2.readData();
-        if (ps2.buttonPressed(PSG_GREEN_FRET))
+    if (error == ps2::ErrorCode::ControllerNotAcceptingCommands) {
+        controller.readData();
+        if (controller.buttonPressed(PSG_GREEN_FRET))
             Serial.println("Green Fret Pressed");
-        if (ps2.buttonPressed(PSG_RED_FRET))
+        if (controller.buttonPressed(PSG_RED_FRET))
             Serial.println("Red Fret Pressed");
-        if (ps2.buttonPressed(PSG_YELLOW_FRET))
+        if (controller.buttonPressed(PSG_YELLOW_FRET))
             Serial.println("Yellow Fret Pressed");
-        if (ps2.buttonPressed(PSG_BLUE_FRET))
+        if (controller.buttonPressed(PSG_BLUE_FRET))
             Serial.println("Blue Fret Pressed");
-        if (ps2.buttonPressed(PSG_ORANGE_FRET))
+        if (controller.buttonPressed(PSG_ORANGE_FRET))
             Serial.println("Orange Fret Pressed");
 
-        if (ps2.buttonPressed(PSG_STAR_POWER))
+        if (controller.buttonPressed(PSG_STAR_POWER))
             Serial.println("Star Power Command");
 
-        if (ps2.buttonPressed(PSG_UP_STRUM))
+        if (controller.buttonPressed(PSG_UP_STRUM))
             Serial.println("Up Strum");
-        if (ps2.buttonPressed(PSG_DOWN_STRUM))
+        if (controller.buttonPressed(PSG_DOWN_STRUM))
             Serial.println("DOWN Strum");
 
-        if (ps2.buttonPressed(PSB_START))
+        if (controller.buttonPressed(PSB_START))
             Serial.println("Start is being held");
-        if (ps2.buttonPressed(PSB_SELECT))
+        if (controller.buttonPressed(PSB_SELECT))
             Serial.println("Select is being held");
 
-        if (ps2.buttonPressed(PSG_ORANGE_FRET)) {
+        if (controller.buttonPressed(PSG_ORANGE_FRET)) {
             Serial.print("Wammy Bar Position:");
-            Serial.println(ps2.analogButtonState(PSG_WHAMMY_BAR), DEC);
+            Serial.println(controller.analogButtonState(PSG_WHAMMY_BAR), DEC);
         }
     } else {
-        ps2.readData(false, vibrate);
-        if (ps2.buttonPressed(PSB_START))
+        controller.readData(false, vibrate);
+        if (controller.buttonPressed(PSB_START))
             Serial.println("Start is being held");
-        if (ps2.buttonPressed(PSB_SELECT))
+        if (controller.buttonPressed(PSB_SELECT))
             Serial.println("Select is being held");
 
-        if (ps2.buttonPressed(PSB_PAD_UP)) {
+        if (controller.buttonPressed(PSB_PAD_UP)) {
             Serial.print("Up held this hard: ");
-            Serial.println(ps2.analogButtonState(PSAB_PAD_UP), DEC);
+            Serial.println(controller.analogButtonState(PSAB_PAD_UP), DEC);
         }
-        if (ps2.buttonPressed(PSB_PAD_RIGHT)) {
+        if (controller.buttonPressed(PSB_PAD_RIGHT)) {
             Serial.print("Right held this hard: ");
-            Serial.println(ps2.analogButtonState(PSAB_PAD_RIGHT), DEC);
+            Serial.println(controller.analogButtonState(PSAB_PAD_RIGHT), DEC);
         }
-        if (ps2.buttonPressed(PSB_PAD_LEFT)) {
+        if (controller.buttonPressed(PSB_PAD_LEFT)) {
             Serial.print("LEFT held this hard: ");
-            Serial.println(ps2.analogButtonState(PSAB_PAD_LEFT), DEC);
+            Serial.println(controller.analogButtonState(PSAB_PAD_LEFT), DEC);
         }
-        if (ps2.buttonPressed(PSB_PAD_DOWN)) {
+        if (controller.buttonPressed(PSB_PAD_DOWN)) {
             Serial.print("DOWN held this hard: ");
-            Serial.println(ps2.analogButtonState(PSAB_PAD_DOWN), DEC);
+            Serial.println(controller.analogButtonState(PSAB_PAD_DOWN), DEC);
         }
 
-        vibrate = ps2.analogButtonState(PSAB_CROSS);
-        if (ps2.buttonsStateChanged()) {
-            if (ps2.buttonPressed(PSB_L3))
+        vibrate = controller.analogButtonState(PSAB_CROSS);
+        if (controller.buttonsStateChanged()) {
+            if (controller.buttonPressed(PSB_L3))
                 Serial.println("L3 pressed");
-            if (ps2.buttonPressed(PSB_R3))
+            if (controller.buttonPressed(PSB_R3))
                 Serial.println("R3 pressed");
-            if (ps2.buttonPressed(PSB_L2))
+            if (controller.buttonPressed(PSB_L2))
                 Serial.println("L2 pressed");
-            if (ps2.buttonPressed(PSB_R2))
+            if (controller.buttonPressed(PSB_R2))
                 Serial.println("R2 pressed");
 
-            if (ps2.buttonPressed(PSB_GREEN))
+            if (controller.buttonPressed(PSB_GREEN))
                 Serial.println("GREEN pressed");
-            if (ps2.buttonPressed(PSB_RED))
+            if (controller.buttonPressed(PSB_RED))
                 Serial.println("RED pressed");
-            if (ps2.buttonPressed(PSB_BLUE))
+            if (controller.buttonPressed(PSB_BLUE))
                 Serial.println("BLUE pressed");
-            if (ps2.buttonPressed(PSB_PINK))
+            if (controller.buttonPressed(PSB_PINK))
                 Serial.println("PINK pressed");
         }
 
-        if (ps2.buttonPressed(PSB_L1) || ps2.buttonPressed(PSB_R1)) {
+        if (controller.buttonPressed(PSB_L1) || controller.buttonPressed(PSB_R1)) {
             Serial.print("Stick Values:");
-            Serial.print(ps2.analogButtonState(PSS_LY), DEC); // Left stick, Y axis. Other options: LX, RY, RX
+            Serial.print(controller.analogButtonState(PSS_LY), DEC); // Left stick, Y axis. Other options: LX, RY, RX
             Serial.print(",");
-            Serial.print(ps2.analogButtonState(PSS_LX), DEC);
+            Serial.print(controller.analogButtonState(PSS_LX), DEC);
             Serial.print(",");
-            Serial.print(ps2.analogButtonState(PSS_RY), DEC);
+            Serial.print(controller.analogButtonState(PSS_RY), DEC);
             Serial.print(",");
-            Serial.println(ps2.analogButtonState(PSS_RX), DEC);
+            Serial.println(controller.analogButtonState(PSS_RX), DEC);
         }
     }
     delay(readControllerDataDelay);
